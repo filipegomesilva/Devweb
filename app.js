11037740f622fd5a4be2291b1cf622fd884a61fd
Barão Rabugento <filipe.gomes@academico.ifg.edu.br>
2023-05-01 18:39:14 -0300

    add bootstrap

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+// OPERADORES
+// && e
+// || ou
+// ! not
+// == comparação
+//!= diferença
+// ++ -- incremento/decremeto
+let nome = 'Filipe'
+
+var h4nome = document.getElementById('nome');
+
+h4nome.innerHTML += " " + nome;
